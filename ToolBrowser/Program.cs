e3aada1ced9eee0e98c0c3e11bbfb8f0b0a663c6
Tyler Mumford <tyler.mumford@icloud.com>
2022-05-02 23:48:31 -0600

    Add toml config support

--- ToolBrowser/Program.cs
+++ ToolBrowser/Program.cs
@@ -1,14 +1,22 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Tomlyn.Extensions.Configuration;
 using ToolBrowser;
 
 var host = new HostBuilder()
     .UseConsoleLifetime()
+    .ConfigureAppConfiguration(ConfigureAppConfiguration)
     .ConfigureServices(ConfigureServices)
     .Build();
 
 var toolBrowser = host.Services.GetRequiredService<IToolBrowser>();
 await toolBrowser.Execute();
+
+void ConfigureAppConfiguration(HostBuilderContext _, IConfigurationBuilder config)
+{
+    config.AddTomlFile("appsettings.toml", true, false);
+}
 
 void ConfigureServices(HostBuilderContext _, IServiceCollection services)
 {
